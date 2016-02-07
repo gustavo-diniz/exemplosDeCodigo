6d54dd0ecdd4517cc18c23485f4640f95c7cf016
gustavo diniz dos santos <gdinsantos1@gmail.com>
2016-02-07 10:31:38 -0200

    incluindo exemplos com testes

--- ExemplosDeCodigo/src/test/java/br/com/exemplo/codigo/AESEncriptadorDecriptadorTest.java
+++ ExemplosDeCodigo/src/test/java/br/com/exemplo/codigo/AESEncriptadorDecriptadorTest.java
@@ -1,13 +1,19 @@
 package br.com.exemplo.codigo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AESEncriptadorDecriptadorTest {
 
-	
+	@Test
 	public void AESEnciptadorTest(){
-	
-		String chave = "";
-		
-		
+		String chave = "abcd1234567efghi";
+		try {
+			String chaveCript = AESEncriptadorDecriptador.AESEncriptar(chave.getBytes(), "senha123");
+			Assert.assertTrue(chaveCript.equals("HxT_rHkZey1Kfc6PPPxzXA"));
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
